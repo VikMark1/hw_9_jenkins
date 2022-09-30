2ceb40364a9a4b4d3961a69e7df9fe75cea37604
viktoriya <tkvika@yandex.ru>
2022-10-01 00:09:20 +0400

    Conftest rework

--- tests/test_student_form.py
+++ tests/test_student_form.py
@@ -5,31 +5,14 @@ from selene.support.shared import browser
 from demoqa_tests.model.data import user
 from demoqa_tests.model.data.user import Gender
 from demoqa_tests.model.pages import registration_form
-from utils import attach
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 
-def test_student_registration_form():
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options)
-    browser.config.driver = driver
-
+def test_student_registration_form(setup_browser):
+    browser = setup_browser
 
     #GIVEN
-    with allure.step("Remove ads"):
-        browser.open('/automation-practice-form')
+    with allure.step("Open form and remove ads"):
+        browser.open('https://demoqa.com/automation-practice-form')
         ads = browser.all('[id^=google_ads][id$=container__]')
         if ads.wait.until(have.size_greater_than_or_equal(3)):
             ads.perform(command.js.remove)
@@ -70,9 +53,3 @@ def test_student_registration_form():
                 ('State and City', 'NCR Delhi')
             ]
         )
-
-
-    attach.add_html(browser)
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_video(browser)
