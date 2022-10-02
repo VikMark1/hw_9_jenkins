1d058fea865b9c72e95ff20a9e8c6aeba73453bb
viktoriya <tkvika@yandex.ru>
2022-10-02 13:26:03 +0400

    Update: conftest, test_student_form

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,36 +1,19 @@
-import os
 from selene.support.shared import browser
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
-from dotenv import load_dotenv
-
 from utils import attach
 
-DEFAULT_BROWSER_VERSION = "100.0"
 
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser():
+    browser.config.window_height = 1500
+    browser.config.window_width = 1800
 
-def pytest_addoption(parser):
-    parser.addoption(
-        '--browser_version',
-        default='100.0'
-    )
-
-
-@pytest.fixture(scope='session', autouse=True)
-def load_env():
-    load_dotenv()
-
-
-@pytest.fixture(scope='function')
-def setup_browser(request):
-    browser_version = request.config.getoption('--browser_version')
-    browser_version = browser_version if browser_version != "" else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": browser_version,
+        "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
@@ -38,16 +21,13 @@ def setup_browser(request):
     }
     options.capabilities.update(selenoid_capabilities)
 
-    login = os.getenv('LOGIN')
-    password = os.getenv('PASSWORD')
-
     driver = webdriver.Remote(
-        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
-    browser = Browser(Config(driver))
+    browser.config.driver = driver
 
-    yield browser
+    yield
 
     attach.add_html(browser)
     attach.add_screenshot(browser)
--- tests/test_student_form.py
+++ tests/test_student_form.py
@@ -1,35 +1,37 @@
-import datetime
 import allure
+from allure_commons.types import Severity
 from selene import have, command
 from selene.support.shared import browser
+from demoqa_tests.model.controls import datepicker
 from demoqa_tests.model.data import user
 from demoqa_tests.model.data.user import Gender
 from demoqa_tests.model.pages import registration_form
 
+@allure.tag('web')
+@allure.severity(Severity.CRITICAL)
+@allure.label('owner', 'VikaM')
+@allure.feature('Student Registration Form')
+@allure.story('Filling form of registration')
+@allure.link('demoqa.com')
 
-def test_student_registration_form(setup_browser):
-    browser = setup_browser
+def test_student_registration_form():
 
-    #GIVEN
     with allure.step("Open form and remove ads"):
         browser.open('https://demoqa.com/automation-practice-form')
         ads = browser.all('[id^=google_ads][id$=container__]')
         if ads.wait.until(have.size_greater_than_or_equal(3)):
             ads.perform(command.js.remove)
 
-    #WHEN
     with allure.step("Fill form"):
         registration_form.fill_firstname('Vika')
         registration_form.fill_lastname('Mark')
         registration_form.fill_email('test@test.com')
         registration_form.fill_gender(user.Gender.Female)
         registration_form.fill_mobile('1234567899')
-        registration_form.fill_birthday(datetime.date(2001, 10, 7))
-        #registration_form.assert_filled_birthday(datetime.date(1991, 10, 7))
-        #registration_form.datepicker.select_date_2()
+        datepicker.select_date_2()
+        #registration_form.fill_birthday(datetime.date(2001, 10, 7))
         registration_form.fill_subjects(('English', 'Maths'))
         registration_form.fill_hobbies(user.Hobby.Music, user.Hobby.Reading)
-        #registration_form.select_picture('../resources/test.png')
         registration_form.fill_address('Test address')
         registration_form.scroll_to_state()
         registration_form.set_state('NCR')
@@ -37,7 +39,7 @@ def test_student_registration_form(setup_browser):
         registration_form.scroll_to_submit()
         registration_form.submit_form()
 
-    #THEN
+
     with allure.step("Check form results"):
         registration_form.should_have_submitted(
             [
@@ -45,7 +47,7 @@ def test_student_registration_form(setup_browser):
                 ('Student Email', 'test@test.com'),
                 ('Gender', 'Female'),
                 ('Mobile', '1234567899'),
-                ('Date of Birth', '07 October,2001'),
+                ('Date of Birth', '07 October,1991'),
                 ('Subjects', 'English, Maths'),
                 ('Hobbies', 'Music, Reading'),
                 #('Picture', 'test.png'),
